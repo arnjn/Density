86fb057007f70d7796dc9465e93cad76fd97e91c
arnjn <124183792+arnjn@users.noreply.github.com>
2024-04-02 17:56:37 -0600

    Add files via upload

--- Density.py
+++ Density.py
@@ -33,12 +33,27 @@ def calculate_density(position, grid ,grid_spacing=1.5):
     a= 1/(grid_spacing**(3))
     return val*(grid_spacing**(-3))
 
+def calculate_reynolds_stress_tensor(positions, forces, velocities, box_volume):
+    reynolds_stress_tensor = np.zeros((3, 3))
+    for i in range(3):
+        for j in range(i, 3):
+            reynolds_stress_tensor[i, j] = np.sum(positions[:, i] * forces[:, j]) + np.sum(positions[:, j] * forces[:, i])
+            reynolds_stress_tensor[i, j] /= box_volume
+            reynolds_stress_tensor[j, i] = reynolds_stress_tensor[i, j]  # reynolds stress tensor is symmetric
+    
+    for i in range(3):
+        for j in range(i, 3):
+            reynolds_stress_tensor[i, j] -= np.sum(velocities[:, i] * velocities[:, j])
+            reynolds_stress_tensor[j, i] = reynolds_stress_tensor[i, j]  # reynolds stress tensor is symmetric
+    
+    return reynolds_stress_tensor
 
 traj = ase.io.read("C:/Users/sande/Desktop/lammps/flow/sphere.dump" , format='lammps-dump-text',index=':')
 grid_spacing = 1.5
 #30,20,20
 den = np.zeros((30,20,20))
 stress = []
+reynolds_stress = []
 d = []
 i = 0
 # frame = traj[0]
@@ -52,14 +67,17 @@ i = 0
 xden = []
 yden = []
 zden = []
-for j in range(0,50):
+for j in range(0,1):
     print(i)
-    frame = traj[i]
+    frame = traj[0]
     i = i+1
     positions = frame.get_positions()
     forces = (frame.get_forces())
+    velocities = frame.get_velocities()
     stress_tensor = calculate_stress_tensor(positions, forces, 12000)
     stress.append(stress_tensor)
+    reynolds_stress_tensor = calculate_reynolds_stress_tensor(positions, forces, velocities, 12000)
+    reynolds_stress.append(reynolds_stress_tensor)
 
     # for z in range(0,20):
     #     for x in range(0,30):
@@ -96,14 +114,15 @@ timesteps = range(0,50)
 # Plot the contour of stress tensor components along different axes
 print((stress))
 
-for i in range(3):
-    plt.figure(figsize=(8, 6))
-    plt.plot(timesteps, [stress_tensor[i, i] for stress_tensor in stress])
-    plt.xlabel('Timestep')
-    plt.ylabel('Stress Component')
-    plt.title(f'Stress Component along {["X", "Y", "Z"][i]} Axis')
+# for i in range(3):
+#     plt.figure(figsize=(8, 6))
+#     plt.plot(timesteps, [stress_tensor[i, i] for stress_tensor in stress])
+#     plt.xlabel('Timestep')
+#     plt.ylabel('Stress Component')
+#     plt.title(f'Stress Component along {["X", "Y", "Z"][i]} Axis')
     
-plt.show()
+# plt.show()
+
 # plt.figure(figsize=(8, 6))
 # plt.plot(timesteps, xden, label='X Axis')
 # plt.plot(timesteps, yden, label='Y Axis')
@@ -128,21 +147,44 @@ plt.show()
 # plt.title('Contour Plot of Density (x-y Projection)')
 # # Create a grid of x and y values
 # # Sum the stress tensors along the 0-axis
-# s = np.sum(np.array(stress), axis=0)
+s = np.sum(np.array(stress), axis=0)
 
 # # Reshape the 3x3 array into a 1D array
-# zp = s.reshape(1, -1)
+zp = s.reshape(1, -1)
 
-# # Create meshgrids for x and y
-# x = np.arange(s.shape[1])
-# y = np.arange(s.shape[0])
-# X, Y = np.meshgrid(x, y)
-# zp2 = zp.reshape(X.shape)
-# # Plot the contour
-# plt.figure(figsize=(8, 6))
-# plt.contourf(X, Y, zp2, cmap='viridis')
-# plt.colorbar(label='Stress')
-# plt.xlabel('X')
-# plt.ylabel('Y')
-# plt.title('Stress Tensor')
-# plt.show()
+# Create meshgrids for x and y
+x = np.arange(s.shape[1])
+y = np.arange(s.shape[0])
+X, Y = np.meshgrid(x, y)
+zp2 = zp.reshape(X.shape)
+# Plot the contour
+plt.figure(figsize=(8, 6))
+plt.contourf(X, Y, zp2, cmap='viridis')
+plt.colorbar(label='Stress')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('Stress Tensor')
+#plt.show()
+
+print("--------------------------------")
+subtracted_list = [x - y for x, y in zip(stress, reynolds_stress)]
+print(subtracted_list)
+
+s = np.sum(np.array(subtracted_list), axis=0)
+
+# # Reshape the 3x3 array into a 1D array
+zp = s.reshape(1, -1)
+
+# Create meshgrids for x and y
+x = np.arange(s.shape[1])
+y = np.arange(s.shape[0])
+X, Y = np.meshgrid(x, y)
+zp2 = zp.reshape(X.shape)
+# Plot the contour
+plt.figure(figsize=(8, 6))
+plt.contourf(X, Y, zp2, cmap='viridis')
+plt.colorbar(label='subtracted_list')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('Stress Tensor')
+plt.show()
